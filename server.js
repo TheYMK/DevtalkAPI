7ff6dd919d2b2f5cd12115ba65e6b8d63bf65c1c
Kaym Kassai <kaymkassai269@gmail.com>
2020-10-24 21:54:08 +0300

    renamed github repo

---
+++ server.js
@@ -0,0 +1,15 @@
+// ============================================================
+//                  Importations
+// ============================================================
+const express = require('express'),
+	morgan = require('morgan'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	cors = require('cors'),
+	mongoose = require('mongoose');
+require('dotenv').config();
+
+// route importations
+
+// express app initialization
+const app = express();
