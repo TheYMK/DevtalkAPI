ec1fa40835ef68720082a8ec87b3c6e0535c2e04
Kaym Kassai <kaymkassai269@gmail.com>
2020-10-24 21:59:55 +0300

    testing git remote link 2

--- server.js
+++ server.js
@@ -17,10 +17,3 @@ const app = express();
 // ============================================================
 //                          Server
 // ============================================================
-
-// port
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-	console.log(`====> Server running on port ${port}...`);
-});
